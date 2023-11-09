cc19b04dd916a9d78fb9a61423d98909ec397526
Ana <anmeuri@gmail.com>
2023-11-09 01:49:30 -0800

    Include timeago library to translate date/time data

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,31 +5,6 @@
  */
 
 $(document).ready(function() {
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd"
-    },
-    "content": {
-      "text": "Je pense, donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-];
 
 const renderTweets = function(arrOfTweets) {
   for (const tweet of arrOfTweets) {
@@ -40,23 +15,18 @@ const renderTweets = function(arrOfTweets) {
 
 const createTweetElement = function(tweetObj) {
 
-  const date = (data) => {
-    const result = new Date(data);
-    return result.toDateString();
-  }
-
   const $tweet = $(
     `<article class="tweets-container">
       <header>
         <span class="user-header">
-          <img src=${tweetObj['user']['avatars']}>
-          <span class="other-user">${tweetObj['user']['name']}</span>
+          <img src=${tweetObj.user.avatars}>
+          <span class="other-user">${tweetObj.user.name}</span>
         </span>
-        <span class="user-handle">${tweetObj['user']['handle']}</span>
+        <span class="user-handle">${tweetObj.user.handle}</span>
       </header>  
-      <p class="tweet-content">${tweetObj['content']['text']}</p>
+      <p class="tweet-content">${tweetObj.content.text}</p>
       <footer>
-        <span>${date(tweetObj['created_at'])}</span>
+        <span>${timeago.format((tweetObj.created_at) - 11 * 1000 * 60 * 60)}</span>
         <span class="reaction-icons" >
           <i class="fa-solid fa-flag"></i>
           <i class="fa-solid fa-retweet"></i>
@@ -69,20 +39,37 @@ const createTweetElement = function(tweetObj) {
   return $tweet;
 }  
 
-renderTweets(data);
+/////////////////////////////////////////////////////
+//        POST request for new tweets
+/////////////////////////////////////////////////////
 
-/////////////////////////////////////////////////////
-//        AJAX POST request for new tweets
-/////////////////////////////////////////////////////
+// $('#tweet-text').on("input", function() {
+//   const $tweetLength = this.value.length;
+//   console.log($tweetLength);
+// })
 
 $('.new-tweet').on("submit", function(event) {
   event.preventDefault();
-  const $serializedData = $(this).children().find('#tweet-text').serialize();
-  $.ajax({
-    type: "POST",
-    url: "/tweets",
-    data: $serializedData,
+  const $data = $(this).children().find('#tweet-text');
+  const $serializedData = $data.serialize();
+  // if ($serializedData.length -5 <= 0) {
+  //   alert("Please enter your tweet before submitting")
+  // }
+  $.post("/tweets", $serializedData)
+
+  });
+
+/////////////////////////////////////////////////////
+//        GET request for tweets in DB
+/////////////////////////////////////////////////////
+
+const loadTweets = function () {
+  $.getJSON("/tweets/", function (data) {
+    renderTweets(data);
   })
-})
+}
+
+loadTweets();
+
 
 });
