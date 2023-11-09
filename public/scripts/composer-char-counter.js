e0d96297c1f949b67c1a8bdf92153339f42af89e
Ana <anmeuri@gmail.com>
2023-11-08 18:16:54 -0800

    Add feed/other users tweets container and styling

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,6 +1,14 @@
 $(document).ready(function() {
   // console.log('document says: im ready')
-  $('#tweet-text').on("input", function() {
+  $('#tweet-text').on("input", function(event) {
+    for (let key in event.target) {
+      if (event.target.hasOwnProperty(key)) {
+        console.log('Own Property', key);
+      } else {
+        console.log('Not Own Property', key);
+      }
+    }
+    // console.log('keys', keys);
     // console.log(this);
     const charactersLeft = 140 - this.value.length;
     // console.log(this.counter.value)
