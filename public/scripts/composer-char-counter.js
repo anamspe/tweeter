a974c2034ae96a96629ad699312f6d3c4376bc63
Ana <anmeuri@gmail.com>
2023-11-08 23:40:28 -0800

    Delete extra lines and functions not needed

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,17 +1,6 @@
 $(document).ready(function() {
-  // console.log('document says: im ready')
-  $('#tweet-text').on("input", function(event) {
-    for (let key in event.target) {
-      if (event.target.hasOwnProperty(key)) {
-        console.log('Own Property', key);
-      } else {
-        console.log('Not Own Property', key);
-      }
-    }
-    // console.log('keys', keys);
-    // console.log(this);
+  $('#tweet-text').on("input", function() {
     const charactersLeft = 140 - this.value.length;
-    // console.log(this.counter.value)
     const $counter = $(this).siblings('div').children('.counter')
     $counter.html(charactersLeft);
     if (charactersLeft < 0)  {
